f3e24849c677813ad4d3659bb683f21df23b076b
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-21 22:30:20 +0530

    under dev

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="h-screen w-full flex items-center justify-center">
       <div className="flex flex-col items-center">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/3225/3225632.png"
-          className="h-16 animate-spin"
+          src="https://icon-library.com/images/icon-gif/icon-gif-3.jpg"
+          className="h-56"
           alt=""
         />
         <p className="mt-9 text-neutral-700">This site is under development</p>
